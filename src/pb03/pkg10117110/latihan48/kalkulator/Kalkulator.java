7fcf7a34f349dac5b42d82fb68f37be462792581
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-10-18 15:11:37 +0700

    first commit

---
+++ src/pb03/pkg10117110/latihan48/kalkulator/Kalkulator.java
@@ -0,0 +1,54 @@
+
+package pb03.pkg10117110.latihan48.kalkulator;
+
+
+public class Kalkulator {
+    private double value1,value2;
+
+    public double getValue1() {
+        return value1;
+    }
+
+    public void setValue1(double value1) {
+        this.value1 = value1;
+    }
+
+    public double getValue2() {
+        return value2;
+    }
+
+    public void setValue2(double value2) {
+        this.value2 = value2;
+    }
+    
+    public void setNameProject(){
+        System.out.println("Project Calculator");
+        
+    }
+    
+    public void setNoteProject(String note){
+        System.out.println("This project shown you how to manage method in java");
+    }
+    
+    public double add(){
+        return value1+value2;
+        
+    }
+    
+    public double minus(){
+        return value1-value2;
+        
+    }
+    
+    public double multiplication(){
+        return value1*value2;
+        
+    }
+    
+    public double division(){
+        return value1/value2;
+        
+    }
+    
+    
+}
---
+++ src/pb03/pkg10117110/latihan48/kalkulator/PB0310117110Latihan48Kalkulator.java
@@ -0,0 +1,35 @@
+
+
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Perhitungan Menggunakan Kalkulator Sederhana Dengan Menggunakan Konsep OOP
+ */
+package pb03.pkg10117110.latihan48.kalkulator;
+
+
+public class PB0310117110Latihan48Kalkulator {
+
+
+    public static void main(String[] args) {
+        
+        Kalkulator objKal = new Kalkulator();
+        objKal.setValue1(7.0);
+        objKal.setValue2(5.0);
+        System.out.println("VALUE 1 = "+objKal.getValue1());
+        System.out.println("VALUE 2 = "+objKal.getValue2());
+        objKal.setNameProject();
+        objKal.setNoteProject("");
+        System.out.println("Result Add is = "+objKal.add());
+        System.out.println("Result Minus is = "+objKal.minus());
+        System.out.println("Result Multiple is = "+objKal.multiplication());
+        System.out.println("Result Division is = "+objKal.division());
+        
+    }
+    
+}
